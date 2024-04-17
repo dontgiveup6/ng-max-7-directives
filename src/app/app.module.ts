d15ebcfd4544445840b518d5060a43035a6d7c38
Georgi Serafimov <g.serafimov@c-trust.eu>
2024-04-17 10:33:44 +0300

    creating a custom structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHightlightDirective } from './better-hightlight/better-hightlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
     BetterHightlightDirective,
+    UnlessDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [provideClientHydration()],
